62303f44b6f38e928c94ec7f1adc59e51404c95c
Julian Espinoza <122400142+julianesva@users.noreply.github.com>
2026-03-31 21:34:21 -0600

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+
+    def dfs(self, heights, prev, r, c, visited): 
+        if (r < 0 or r >= len(heights) or c < 0 or c >= len(heights[0]) or heights[r][c] < prev or (r, c) in visited):
+            return
+        
+        visited.add((r,c))
+
+        self.dfs(heights, heights[r][c], r+1, c, visited)
+        self.dfs(heights, heights[r][c], r-1, c, visited)
+        self.dfs(heights, heights[r][c], r, c+1, visited)
+        self.dfs(heights, heights[r][c], r, c-1, visited)
+
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+
+        result = []
+
+        rows, cols = len(heights), len(heights[0])
+        pacific, atlantic = set(), set()
+
+        for c in range(cols):
+            self.dfs(heights, heights[0][c], 0, c, pacific)
+            self.dfs(heights, heights[rows-1][c], rows-1, c, atlantic)
+
+        for r in range(rows):
+            self.dfs(heights, heights[r][0], r, 0, pacific)
+            self.dfs(heights, heights[r][cols-1], r, cols-1, atlantic)
+
+        for row in range(rows):
+            for col in range(cols):
+                if((row,col) in atlantic and (row,col) in pacific):
+                    result.append((row,col))
+
+        return result
+        
